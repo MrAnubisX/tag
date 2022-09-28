3c6cd44249d7d38c6660cc96f5c77835976e3553
MrAnubisX <105823816+MrAnubisX@users.noreply.github.com>
2022-09-27 22:57:09 -0400

    refactor: removed xcodeproj and setup for cmake

---
+++ Tag/array.c
@@ -0,0 +1,8 @@
+//
+// array.c
+// Tag
+//
+// Created by Christopher Snead on 2022/09/28.
+//
+
+#include "array.h"
---
+++ Tag/array.h
@@ -0,0 +1,11 @@
+//
+// array.h
+// Tag
+//
+// Created by Christopher Snead on 2022/09/28.
+//
+
+#ifndef TAG_TAG_ARRAY_H_
+#define TAG_TAG_ARRAY_H_
+
+#endif //TAG_TAG_ARRAY_H_
